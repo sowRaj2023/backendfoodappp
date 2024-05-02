1b22aaa8786a0c66470eca0363b3eb11dd7d69f9
sowRaj2023 <sownyuvan1180@gmail.com>
2024-05-02 15:51:06 +0530

    commit message

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const path = require("path");
 const {open}  = require("sqlite");
 const sqlite3 = require("sqlite3")
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken")
+const cors = require("cors")
+
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 const dbPath = path.join(__dirname, "user.db");
 
@@ -15,7 +19,6 @@ let db;
 const initializeDbAndServer =  async() => {
     try {
         db= await open({
-
             filename:dbPath,
             driver:sqlite3.Database,
         });
@@ -31,10 +34,14 @@ const initializeDbAndServer =  async() => {
 };
 initializeDbAndServer();
 
-
 //get user API
 
-app.get("/user/", async (request, response) =>{
+app.get("/", (request,response) => {
+  response.send("checking");
+})
+
+//get user details
+app.get("/user", async (request, response) =>{
 
     const getUserQuery = `
     select
@@ -49,7 +56,6 @@ app.get("/user/", async (request, response) =>{
 });
 
 //Register API
-
 app.post("/register", async (request,response) =>{
   const{username,name,password,gender,location} = request.body;
   const hashedPassword = await bcrypt.hash(password, 12);
@@ -81,3 +87,74 @@ app.post("/register", async (request,response) =>{
     response.status(400).send("user already exist");
   }
 });
+
+//login API
+app.post("/login", async (request,response) => {
+ try{
+  const {password,username} = request.body;
+  const selectUser = `
+  select
+  *
+  from
+  userDb
+  where
+  username = '${username}'`;
+  const userData  = await db.get(selectUser)
+
+  if(!userData){
+    return response.status(400).send("Invalid Username or Password")
+  }
+  const passwordMatch = await bcrypt.compare(password, userData.password);
+
+  if(passwordMatch){
+    const jwt_token = await jwt.sign({username},"sowndharya" )
+    return response.send({jwt_token})
+  }else{
+    return response.status(400).send("Invalid Username or Password");
+  }
+
+}catch(error){
+  console.error("Login Error:", error)
+  response.status(500).send("Internal Server Error")
+}
+});
+
+//get menu items
+
+app.get("/menu", async (request, response) =>{
+
+  
+ 
+   const getMenuQuery = `
+   select
+   *
+   from 
+   menuDb
+   order by
+   price ASC
+   ;`;
+   const getMenuArray = await db.all(getMenuQuery);
+   response.send(getMenuArray);
+ 
+ });
+
+
+//get prime items
+
+app.get("/prime", async (request, response) =>{
+
+  const getPrimeQuery = `
+  select
+  *
+  from 
+  primeDb
+  order by
+  id;`;
+  const getPrimeArray = await db.all(getPrimeQuery);
+  response.send(getPrimeArray);
+
+});
+
+
+
+
